1777532d9f563b9a1576f08fa8f2226559d49098
ka-weihe <k@weihe.dk>
2020-07-22 16:21:55 +0200

    Make it 15 times faster (#30)
    * 10 times faster
    
    * minified
    
    * fix grunt
    
    * fix ci
    
    * remove outdated ci
    
    * remove package.lock.json

--- levenshtein.js
+++ levenshtein.js
@@ -7,6 +7,9 @@
   } catch (err){
     console.log("Collator could not be initialized and wouldn't be used");
   }
+
+  var levenshtein = require('fastest-levenshtein');
+
   // arrays to re-use
   var prevRow = [],
     str2Char = [];
@@ -27,25 +30,25 @@
     get: function(str1, str2, options) {
       var useCollator = (options && collator && options.useCollator);
       
-      var str1Len = str1.length,
-        str2Len = str2.length;
-      
-      // base cases
-      if (str1Len === 0) return str2Len;
-      if (str2Len === 0) return str1Len;
-
-      // two rows
-      var curCol, nextCol, i, j, tmp;
-
-      // initialise previous row
-      for (i=0; i<str2Len; ++i) {
-        prevRow[i] = i;
-        str2Char[i] = str2.charCodeAt(i);
-      }
-      prevRow[str2Len] = str2Len;
-
-      var strCmp;
       if (useCollator) {
+        var str1Len = str1.length,
+          str2Len = str2.length;
+        
+        // base cases
+        if (str1Len === 0) return str2Len;
+        if (str2Len === 0) return str1Len;
+
+        // two rows
+        var curCol, nextCol, i, j, tmp;
+
+        // initialise previous row
+        for (i=0; i<str2Len; ++i) {
+          prevRow[i] = i;
+          str2Char[i] = str2.charCodeAt(i);
+        }
+        prevRow[str2Len] = str2Len;
+
+        var strCmp;
         // calculate current row distance from previous row using collator
         for (i = 0; i < str1Len; ++i) {
           nextCol = i + 1;
@@ -76,40 +79,9 @@
           // copy last col value into previous (in preparation for next iteration)
           prevRow[j] = nextCol;
         }
+        return nextCol;
       }
-      else {
-        // calculate current row distance from previous row without collator
-        for (i = 0; i < str1Len; ++i) {
-          nextCol = i + 1;
-
-          for (j = 0; j < str2Len; ++j) {
-            curCol = nextCol;
-
-            // substution
-            strCmp = str1.charCodeAt(i) === str2Char[j];
-
-            nextCol = prevRow[j] + (strCmp ? 0 : 1);
-
-            // insertion
-            tmp = curCol + 1;
-            if (nextCol > tmp) {
-              nextCol = tmp;
-            }
-            // deletion
-            tmp = prevRow[j + 1] + 1;
-            if (nextCol > tmp) {
-              nextCol = tmp;
-            }
-
-            // copy current col value into previous (in preparation for next iteration)
-            prevRow[j] = curCol;
-          }
-
-          // copy last col value into previous (in preparation for next iteration)
-          prevRow[j] = nextCol;
-        }
-      }
-      return nextCol;
+      return levenshtein.distance(str1, str2);
     }
 
   };
@@ -133,4 +105,3 @@
     window.Levenshtein = Levenshtein;
   }
 }());
-
